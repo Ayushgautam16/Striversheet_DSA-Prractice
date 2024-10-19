5cd097aec4abf516d3a9f07fbfe660dbe4013bf6
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-10-19 09:38:56 +0530

    dd

--- basics.cpp
+++ basics.cpp
@@ -1,7 +1,15 @@
-#include<iostream>
-#include<math.h>
-using namespace std;
-int main(){
+// #include<iostream>
+// #include<math.h>
+// using namespace std;
+// int main(){
+//     cout <<"Hello World!";
+//     cout << "\n";
+//     cout << "This is a C++ program!";
+//     cout << "\n";
+//     cout << "Today's date is " << __DATE__ << " and time is " << __TIME__;
+// cout <<"hey ayush how is it going ";
+// return 0;
+// }
 
-return 0;
-}
+// As you can see, the newline character \n inserts a line break, but the second "Hey, Striver!" is still on the same line as the first one.
+// You can also use std::endl to insert a newline character and flush the output buffer. Here's the code and its corresponding terminal output:
