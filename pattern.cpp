60c49854d744d6652e0bdc1d06a929bd79eacb96
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-10-19 15:38:08 +0530

    1

---
+++ pattern.cpp
@@ -0,0 +1,22 @@
+// ****  ****pattern printing******
+// ****
+// ****
+// ****
+
+#include <iostream>
+using namespace std;
+int main()
+{int i,j, n;
+    cout <<"enter the the num of time you want to print star patterns"<<endl;
+cin >> n;
+
+    for(i = 0; i <n; i++){
+        for ( j = 0; j < n; j++)
+        {
+            /* code */
+            cout << "* ";
+        }
+        cout<<endl; 
+
+    }
+}
