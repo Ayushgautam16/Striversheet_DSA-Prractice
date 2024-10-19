3ad5ccddaf85cf8ae5d23d3abed03615bdaa62f0
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-10-19 15:20:18 +0530

    22

--- basics.cpp
+++ basics.cpp
@@ -47,3 +47,35 @@
 
 // #include<bits/stdc++.h>.
 // The bits/stdc++.h header is a shortcut that includes a vast number of standard C++ libraries, making it easier to access a wide range of functions and classes without specifying each library individually. It's a time-saving approach for programmers, especially when you need several standard libraries in your code.
+#include <bits/stdc++.h>
+using namespace std;
+
+void pattern1(int N)
+{
+    // This is the outer loop which will loop for the rows.
+    for (int i = 0; i < N; i++)
+    {
+        // This is the inner loop which here, loops for the columns
+        // as we have to print a rectangular pattern.
+        for (int j = 0; j < N; j++)
+        {
+            cout << "* ";
+        }
+       
+        // As soon as N stars are printed, we move to the
+        // next row and give a line break otherwise all stars
+        // would get printed in 1 line.
+        cout << endl;
+    }
+}
+
+int main()
+{   
+    // Here, we have taken the value of N as 5.
+    // We can also take input from the user.
+    int N = 5;
+
+    pattern1(N);
+
+    return 0;
+}
