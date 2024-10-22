479ccaa863c499e07f3ef20772bb767de4a925a2
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-10-22 22:14:24 +0530

    33

--- basicMaths.cpp
+++ basicMaths.cpp
@@ -1,9 +1,30 @@
+// #include<iostream>
+// using namespace std;
+
+// // code for the reverse num
+
+// int main(){
+//     int n;
+//     cin >> n;
+//     int revNum =0;
+//     while (n > 0)
+    
+//     {
+//        int ld = n%10;
+//        revNum = (revNum*10) +ld;
+//        n= n/10;/* code */
+//     }
+//     cout << revNum ;
+    
+// return 0;
+// }
+
+
 #include<iostream>
 using namespace std;
 
 
-
 int main(){
-    
+
 return 0;
 }
