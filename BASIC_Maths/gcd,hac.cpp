44347ac1580b56016b0d22b2a7127e68e1e06c10
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-10-24 00:22:59 +0530

    11

--- BASIC_Maths/gcd,hac.cpp
+++ BASIC_Maths/gcd,hac.cpp
@@ -1,16 +1,16 @@
 // checking the gcd and hcf of the number greatest dommon division
 
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
-int GCD(int n){
-    if(n == 0)
-        return 0;
-    else
-        return GCD(n%10, n/10);
-}
-int main(){
+// int GCD(int n){
+//     if(n == 0)
+//         return 0;
+//     else
+//         return GCD(n%10, n/10);
+// }
+// int main(){
 
 
-return 0;
+// return 0;
 }
