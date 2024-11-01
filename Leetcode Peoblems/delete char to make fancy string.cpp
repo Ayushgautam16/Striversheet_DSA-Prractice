b1bc0a558680df37b33ddec7750c8ea3a459051e
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-11-01 10:22:20 +0530

    1

--- Leetcode Peoblems/delete char to make fancy string.cpp
+++ Leetcode Peoblems/delete char to make fancy string.cpp
@@ -6,10 +6,10 @@ public:
         string ans;
         for(auto &ch:s)
         {
-            n = ans.size();
-            if(n > 1 and ans.back() == ch and ans.back() == ans[n - 2])
-            {
-                continue;
+            // n = ans.size();
+            // if(n > 1 and ans.back() == ch and ans.back() == ans[n - 2])
+            // {
+            //     continue;
         //     }
         //     ans += ch;
         // }
