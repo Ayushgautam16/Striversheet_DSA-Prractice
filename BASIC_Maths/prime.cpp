ea075eb23e7d8478d1840098e448d3f7ecb33236
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-11-08 00:56:14 +0530

    1

--- BASIC_Maths/prime.cpp
+++ BASIC_Maths/prime.cpp
@@ -4,7 +4,7 @@
 
 // int isPrime(int num){
     // if(num <= 1)
-        return false;
+        // return false;
     for(int i = 2; i <= sqrt(num); i++){
         if(num % i == 0)
             return false;
