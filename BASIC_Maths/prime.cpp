e64f45dbe6cb83125a163945189ea37d02ca9b23
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-11-09 22:22:25 +0530

    1

--- BASIC_Maths/prime.cpp
+++ BASIC_Maths/prime.cpp
@@ -13,30 +13,30 @@
 // }
 // int main(){
     int n;
-    cout << "Enter a number: ";
-    cin >> n;
+    // cout << "Enter a number: ";
+    // cin >> n;
     
-    // check if the number is prime 
-    if(isPrime(n)){
-        cout << n << " is a prime number." << endl;
-    } else {
-        cout << n << " is not a prime number." << endl;
-    }
+    // // check if the number is prime 
+    // if(isPrime(n)){
+    //     cout << n << " is a prime number." << endl;
+    // } else {
+    //     cout << n << " is not a prime number." << endl;
+    // }
     
-    // calculate sum of digits
-    int sum = 0;
-    while(n!= 0){
-        sum += n % 10;
-        n /= 10;
-    }
+    // // calculate sum of digits
+    // int sum = 0;
+    // while(n!= 0){
+    //     sum += n % 10;
+    //     n /= 10;
+    // }
     
-    cout << "Sum of digits: " << sum << endl;
+    // cout << "Sum of digits: " << sum << endl;
     
-    // find the largest prime number less than or equal to the sum
-    int largestPrime = sum;
-    while(!isPrime(largestPrime)){
-        largestPrime--;
-    }
+    // // find the largest prime number less than or equal to the sum
+    // int largestPrime = sum;
+    // while(!isPrime(largestPrime)){
+    //     largestPrime--;
+    // }
     
 //     cout << "Largest prime number less than or equal to the sum: " << largestPrime << endl;
     
