5b005ece064892435956f25b71426c220cbf897c
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-11-09 22:21:10 +0530

    1

--- BASIC_Maths/prime.cpp
+++ BASIC_Maths/prime.cpp
@@ -7,7 +7,7 @@
     //     // return false;
     // for(int i = 2; i <= sqrt(num); i++){
         // if(num % i == 0)
-            return false;
+            // return false;
     }
     return true;
 }
