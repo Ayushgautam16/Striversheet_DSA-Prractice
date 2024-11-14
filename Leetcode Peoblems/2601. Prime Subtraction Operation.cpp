aa6051180d1786a687e6091bc9ea55ba2e83cb5d
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-11-14 23:31:35 +0530

    1

--- Leetcode Peoblems/2601. Prime Subtraction Operation.cpp
+++ Leetcode Peoblems/2601. Prime Subtraction Operation.cpp
@@ -6,8 +6,8 @@
 
 // using namespace std;
 // class Solution {
-    void sieveOfEratosthenes(vector<int>& primes, int maxVal) {
-        bool sieve[maxVal + 1];
+    // void sieveOfEratosthenes(vector<int>& primes, int maxVal) {
+    //     bool sieve[maxVal + 1];
         memset(sieve, false, sizeof(sieve));
         for (int i = 2; i * i < maxVal; ++i) {
             if (sieve[i] == false) {
