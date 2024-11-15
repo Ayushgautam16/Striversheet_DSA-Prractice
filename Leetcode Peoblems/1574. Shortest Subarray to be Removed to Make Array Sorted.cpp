0372bbdeb88bae7f5b1fffc4ddf2d003e8033350
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-11-15 16:07:38 +0530

    1

--- Leetcode Peoblems/1574. Shortest Subarray to be Removed to Make Array Sorted.cpp
+++ Leetcode Peoblems/1574. Shortest Subarray to be Removed to Make Array Sorted.cpp
@@ -4,4 +4,6 @@ public:
     int findLengthOfShortestSubarray(vector<int>& arr) {
         int n = arr.size(), l{0}, r = n - 1;
         while (l + 1 < n && arr[l] <= arr[l + 1]) ++l;
-     
+        if (l == n - 1) return 0;
+        while (r > l && arr[r - 1] <= arr[r]) --r;
+      
