71044bf834b2d78aaa7a6cb31595c5d0d75ed4e9
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-11-16 22:04:06 +0530

    1

--- Leetcode Peoblems/3254. Find the Power of K-Size Subarrays.cpp
+++ Leetcode Peoblems/3254. Find the Power of K-Size Subarrays.cpp
@@ -1,4 +1,17 @@
 // 3254. Find the Power of K-Size Subarrays I
 class Solution {
 public:
-   
+    vector<int> resultsArray(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> ans(n - k + 1, -1); 
+
+        int conseCnt = 1;
+        for (int i = 1; i < k; i++) {
+            if (nums[i] == nums[i - 1] + 1) {
+                conseCnt++;
+            } else {
+                conseCnt = 1;
+            }
+        }
+
+      
