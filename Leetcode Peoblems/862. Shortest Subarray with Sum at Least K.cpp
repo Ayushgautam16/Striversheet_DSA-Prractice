9b159ce7f2724b5e17518213c8e15caca62909e9
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-11-17 23:47:37 +0530

    1

--- Leetcode Peoblems/862. Shortest Subarray with Sum at Least K.cpp
+++ Leetcode Peoblems/862. Shortest Subarray with Sum at Least K.cpp
@@ -1,1 +1,10 @@
 862. Shortest Subarray with Sum at Least K
+class Solution {
+public:
+    int shortestSubarray(vector<int>& nums, int k) {
+        int s=0,j=0,m=INT_MAX;
+        for(int i=0;i<nums.size();i++){
+            s+=nums[i];
+            if(s<0){
+                s=0;
+                j=i+1;
