35f1af2172a7780974e51f7c3a214369fe217f55
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-11-18 08:52:30 +0530

    1

--- Leetcode Peoblems/1652. Defuse the Bomb.cpp
+++ Leetcode Peoblems/1652. Defuse the Bomb.cpp
@@ -1,1 +1,16 @@
 // 1652. Defuse the Bomb
+class Solution {
+public:
+    void addNums(vector<int> &nums, vector<int> &code)
+    {
+        for(auto val:code) 
+            nums.push_back(val);
+    }
+    vector<int> decrypt(vector<int>& code, int k) 
+    {
+        int n = code.size();
+        vector<int>ans(n);
+        if(k == 0) return ans;
+        auto nums = code;
+        addNums(nums, code);
+        addNums(nums, code);
