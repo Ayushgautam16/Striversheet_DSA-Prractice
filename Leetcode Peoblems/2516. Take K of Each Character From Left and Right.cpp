4ffe51021cedabe62415e634a792262a7f72f7f5
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-11-20 21:40:54 +0530

    1

--- Leetcode Peoblems/2516. Take K of Each Character From Left and Right.cpp
+++ Leetcode Peoblems/2516. Take K of Each Character From Left and Right.cpp
@@ -1,4 +1,12 @@
 // 2516. Take K of Each Character From Left and Right
 class Solution {
 public:
+    static int takeCharacters(string& s, int k) {
+        const int n=s.size();
+        int freq[3]={0};
+        for(char c: s)
+            freq[c-'a']++;
+        if (any_of(freq, freq+3, [k](int f){ return f<k;}))
+            return -1;
+    //    print(freq);
    
