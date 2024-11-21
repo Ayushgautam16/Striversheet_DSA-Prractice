db1023805fe81dd9dab784b31aed5da76aa4e62b
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-11-21 10:43:41 +0530

    1

--- Leetcode Peoblems/2516. Take K of Each Character From Left and Right.cpp
+++ Leetcode Peoblems/2516. Take K of Each Character From Left and Right.cpp
@@ -1,19 +1,19 @@
-// 2516. Take K of Each Character From Left and Right
-class Solution {
-public:
-    static int takeCharacters(string& s, int k) {
-        const int n=s.size();
-        int freq[3]={0};
-        for(char c: s)
-            freq[c-'a']++;
-        if (any_of(freq, freq+3, [k](int f){ return f<k;}))
-            return -1;
-    //    print(freq);
-        int ans=n;
-        for(int l=0, r=0; r<n; r++){
-            int x=s[r]-'a';
-            freq[x]--;
-            while (l<=r && any_of(freq, freq+3, [k](int f){ return f<k;})){
-                freq[s[l]-'a']++;
-                l++;
+// // 2516. Take K of Each Character From Left and Right
+// class Solution {
+// public:
+//     static int takeCharacters(string& s, int k) {
+//         const int n=s.size();
+//         int freq[3]={0};
+//         for(char c: s)
+//             freq[c-'a']++;
+//         if (any_of(freq, freq+3, [k](int f){ return f<k;}))
+//             return -1;
+//     //    print(freq);
+//         int ans=n;
+//         for(int l=0, r=0; r<n; r++){
+//             int x=s[r]-'a';
+//             freq[x]--;
+//             while (l<=r && any_of(freq, freq+3, [k](int f){ return f<k;})){
+//                 freq[s[l]-'a']++;
+//                 l++;
   
