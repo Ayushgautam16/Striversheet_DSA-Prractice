9f37b8fbf33722cc01e1f7901be63d4922faf3d1
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-11-22 19:54:39 +0530

    1

--- Leetcode Peoblems/1072. Flip Columns For Maximum Number of Equal Rows.cpp
+++ Leetcode Peoblems/1072. Flip Columns For Maximum Number of Equal Rows.cpp
@@ -5,4 +5,13 @@ public:
     string f(auto& v) {
 //         string r;
         for (auto x : v)
- 
+            r += (x == v[0]);
+        return r;
+    };
+    
+    int maxEqualRowsAfterFlips(vector<vector<int>>& m) {
+        map<string, int> mp; int mx = 0;
+        for (auto& i : m)  mx = max(mx, ++mp[f(i)]);
+        return mx;
+    }
+};
