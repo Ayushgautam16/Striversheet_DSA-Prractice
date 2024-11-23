1620c3e5ebb26ff11a5ae490bf0ea4b28d4bb78f
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-11-23 19:19:21 +0530

    1

--- Leetcode Peoblems/1861. Rotating the Box.cpp
+++ Leetcode Peoblems/1861. Rotating the Box.cpp
@@ -3,4 +3,22 @@ class Solution {
 public:
     void swapChar(vector<char> &vec)
     {
-       
+        int n = vec.size(), Hash = 0;
+        unordered_map<int, int>store;
+        for(int i = 0; i < n; i++)
+        {
+            Hash += vec[i] == '#';
+            if(vec[i] == '*')
+                store[i] = Hash, Hash = 0;
+        }
+        if(Hash) store[n] = Hash;
+
+        for(auto [idx_i, count] : store)
+        {
+            int idx = idx_i;
+            while(count--) vec[--idx] = '#';
+            while(idx-- and vec[idx] != '*') vec[idx] = '.';
+        }
+    }
+
+    
