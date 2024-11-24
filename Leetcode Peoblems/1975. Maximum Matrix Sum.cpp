a0f2e232222e65c51f0c3a6b3c2c68b7aacaa077
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-11-24 14:13:39 +0530

    1

--- Leetcode Peoblems/1975. Maximum Matrix Sum.cpp
+++ Leetcode Peoblems/1975. Maximum Matrix Sum.cpp
@@ -6,4 +6,7 @@ public:
         long long sum = 0; 
         int minus = INT_MAX, amt = 0;
         bool zero = false;
-        
+        for(int i = 0; i < matrix.size(); i++){
+            for(int j = 0; j < matrix[0].size(); j++){
+                int tmp = matrix[i][j];
+                
