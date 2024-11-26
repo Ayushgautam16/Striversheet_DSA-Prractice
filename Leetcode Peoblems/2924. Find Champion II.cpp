7e499ca0babf8189f5baa9a954632558eb1a3e3d
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-11-26 17:27:30 +0530

    1

--- Leetcode Peoblems/2924. Find Champion II.cpp
+++ Leetcode Peoblems/2924. Find Champion II.cpp
@@ -4,4 +4,8 @@ class Solution {
 public:
     int findChampion(int n, vector<vector<int>>& edges) 
     {
+       vector<int> count(n);
+        for(auto ed : edges)
+            count[ed[1]] += 1;
+
         
