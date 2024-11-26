bd4cc95b5ede38612ab65522a2bb90c0b51fbf63
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-11-26 17:27:56 +0530

    1

--- Leetcode Peoblems/2924. Find Champion II.cpp
+++ Leetcode Peoblems/2924. Find Champion II.cpp
@@ -8,4 +8,16 @@ public:
         for(auto ed : edges)
             count[ed[1]] += 1;
 
-        
+        int cnt = 0, ans;
+        for(int i = 0; i < n; i++)
+        {
+            if(count[i] == 0)
+            {
+                ans = i;
+                cnt += 1;
+            }
+            if(cnt == 2) return -1;
+        }
+        return ans;
+    }
+}; 
