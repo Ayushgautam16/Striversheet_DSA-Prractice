ced4c47764b050842b8af94ae8ca24f296e641ee
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-11-27 12:54:11 +0530

    1

--- Leetcode Peoblems/3243. Shortest Distance After Road Addition Queries I.cpp
+++ Leetcode Peoblems/3243. Shortest Distance After Road Addition Queries I.cpp
@@ -2,4 +2,14 @@
 
 class Solution {
 public:
-   
+    int findshortest(vector<pair<int,vector<int>>>adj ,int st,int end,int n){
+      vector<int> dis(n, INT_MAX);
+        dis[st] = 0;        
+        queue<int> q;  
+        q.push(st);        
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();            
+            for (auto p : adj[node].second) { 
+               
+                
