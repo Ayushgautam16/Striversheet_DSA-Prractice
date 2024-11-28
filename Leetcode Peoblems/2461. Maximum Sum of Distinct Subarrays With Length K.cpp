ec155fc96e7f4dba1c92fb0476cb88207b908f56
AYUSH GAUTAM <142661926+Ayushgautam16@users.noreply.github.com>
2024-11-28 08:33:10 +0530

    Update 2461. Maximum Sum of Distinct Subarrays With Length K.cpp

--- Leetcode Peoblems/2461. Maximum Sum of Distinct Subarrays With Length K.cpp
+++ Leetcode Peoblems/2461. Maximum Sum of Distinct Subarrays With Length K.cpp
@@ -1,5 +1,5 @@
 // 2461. Maximum Sum of Distinct Subarrays With Length K
-class Solution {
+//class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
         long long n = nums.size(), ws = 0, mx = 0;
