074e43b8f8327b92515472e0c1cf1d7d7b655f2e
AYUSH GAUTAM <142661926+Ayushgautam16@users.noreply.github.com>
2024-11-28 08:34:27 +0530

    Update 2461. Maximum Sum of Distinct Subarrays With Length K.cpp

--- Leetcode Peoblems/2461. Maximum Sum of Distinct Subarrays With Length K.cpp
+++ Leetcode Peoblems/2461. Maximum Sum of Distinct Subarrays With Length K.cpp
@@ -5,7 +5,7 @@
       //  long long n = nums.size(), ws = 0, mx = 0;
         unordered_map<int, int> mp;
 
-        for (int i = 0; i < n; i++) {
+    //    for (int i = 0; i < n; i++) {
             // Add the current element to the window
             mp[nums[i]]++;
             ws += nums[i];
