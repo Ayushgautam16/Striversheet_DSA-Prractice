708a332693c83cff5323e0e5ee7f89ede8c7c8a5
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-12-02 09:05:04 +0530

    1

--- Leetcode Peoblems/1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence.cpp
+++ Leetcode Peoblems/1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence.cpp
@@ -2,4 +2,17 @@
 
 class Solution {
 public:
-    
+    int isPrefixOfWord(string sentence, string searchWord) {
+        vector<string> words; // To store the words
+
+        // Create a string stream from the sentence
+        istringstream stream(sentence);
+        string word;
+        int cnt=0;
+        // Extract words from the stream
+        while (stream >> word) {
+            cnt++;
+            if(word.compare(0, searchWord.size(), searchWord) == 0){
+                return cnt;
+            }
+     
