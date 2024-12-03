775a4d245be715d3d9e9a8abf8a8324d5f6bbbad
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-12-03 21:14:47 +0530

    1

--- Leetcode Peoblems/2109. Adding Spaces to a String.cpp
+++ Leetcode Peoblems/2109. Adding Spaces to a String.cpp
@@ -7,4 +7,13 @@ public:
         string ans;
         int i = 0, j = 0;
         int n = s.size(), m = spaces.size();
-        
+        while(j < m)
+        {
+            if(i == spaces[j])
+                ans += ' ', j++;
+            ans += s[i++];
+        }
+        while(i < n) ans += s[i++];
+        return ans;
+    }
+};
