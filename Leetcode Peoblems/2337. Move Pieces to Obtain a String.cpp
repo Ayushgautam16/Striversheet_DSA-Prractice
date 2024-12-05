9c6c1c1b7624c79ad46c9d66deae8c8be993f396
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-12-05 21:26:29 +0530

    1

--- Leetcode Peoblems/2337. Move Pieces to Obtain a String.cpp
+++ Leetcode Peoblems/2337. Move Pieces to Obtain a String.cpp
@@ -1,2 +1,13 @@
 // 2337. Move Pieces to Obtain a String
 
+class Solution {
+public:
+    bool canChange(string start, string target) 
+    {
+        int idx = 0, cur = 0, n = start.size();
+
+        for(int i = 0; i < n; i++)
+        {
+            if(start[i] == target[i]) continue;
+            
+            
