9954e727692b6203b8b78c1aa5530468d8c27fe6
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-12-07 00:46:33 +0530

    1

--- Leetcode Peoblems/2554. Maximum Number of Integers to Choose From a Range I.cpp
+++ Leetcode Peoblems/2554. Maximum Number of Integers to Choose From a Range I.cpp
@@ -2,4 +2,8 @@
 
 class Solution {
 public:
-    
+    int maxCount(vector<int>& banned, int n, int maxSum) {
+        unordered_set<int> ans(banned.begin(),banned.end());//to check in O(1) complexity if range of number is in banned or not
+        int count=0;  //For counting integers
+        int sum=0;  //to check the sum exceed or not
+        
