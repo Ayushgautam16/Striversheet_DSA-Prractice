a48a310ff4056e943a294e5d5c77d508f7095424
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-12-08 22:39:33 +0530

    1

--- Leetcode Peoblems/2054. Two Best Non-Overlapping Events.cpp
+++ Leetcode Peoblems/2054. Two Best Non-Overlapping Events.cpp
@@ -1,2 +1,11 @@
 // 2054. Two Best Non-Overlapping Events1
 
+class Solution {
+public:
+    int maxTwoEvents(vector<vector<int>>& x) {
+        sort(x.begin(), x.end());
+
+        int n = x.size();
+        vector<int>suf(n + 1);
+
+        
