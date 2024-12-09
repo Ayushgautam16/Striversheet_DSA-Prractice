f523b0b8365c81134ec1774e41cbbdc8037dd7ee
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-12-09 20:01:35 +0530

    1

--- Leetcode Peoblems/3152. Special Array II.cpp
+++ Leetcode Peoblems/3152. Special Array II.cpp
@@ -5,4 +5,11 @@ public:
     vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) 
     {
         int n = nums.size();
+        set<int> store;
+        for(int i = 1; i < n; i++)
+            if((nums[i] % 2) == nums[i - 1] % 2)
+                store.insert(i);
+
+        vector<bool> ans;
+        for(auto querie : queries)
         
