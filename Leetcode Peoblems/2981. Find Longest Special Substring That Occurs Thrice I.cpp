33fd71a60c9aad83bb52f82ed12fc851ada3d299
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-12-10 08:37:04 +0530

    1

--- Leetcode Peoblems/2981. Find Longest Special Substring That Occurs Thrice I.cpp
+++ Leetcode Peoblems/2981. Find Longest Special Substring That Occurs Thrice I.cpp
@@ -3,4 +3,17 @@
 class Solution {
 public:
     // time/space: O(n ^ 3)/O(1)
+    int maximumLength(string s) {
+        int n = s.size();
+        int result = -1;
+        // try all possible special substring
+        for (char c = 'a'; c <= 'z'; c++) {
+            for (int len = 1; len <= n; len++) {
+                // get the occurrence of the special substring
+                if (getOccurrence(s, c, len) >= 3) result = max(result, len);
+            }
+        }
+        return result;
+    }
+private:
     
