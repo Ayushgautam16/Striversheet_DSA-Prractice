fae8c89f57efe4b07d6ab8ce719e982c6cda281c
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-12-11 21:25:42 +0530

    1

--- Leetcode Peoblems/2779. Maximum Beauty of an Array After Applying Operation.cpp
+++ Leetcode Peoblems/2779. Maximum Beauty of an Array After Applying Operation.cpp
@@ -1,2 +1,8 @@
 2779. Maximum Beauty of an Array After Applying Operation
 
+class Solution {
+public:
+    int maximumBeauty(vector<int>& nums, int k) {
+        
+        // Find the minimum and maximum values
+        
