53ac364e5b617d6ac702af51cb6a33dcbc8c4ad2
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-12-15 06:00:18 +0530

    1

--- Leetcode Peoblems/1792. Maximum Average Pass Ratio.cpp
+++ Leetcode Peoblems/1792. Maximum Average Pass Ratio.cpp
@@ -1,2 +1,9 @@
 // 1792. Maximum Average Pass Ratio
 
+class Solution {
+public:
+    double maxAverageRatio(vector<vector<int>>& classes, int k) {
+        priority_queue<pair<double,int>> pq;
+        for(int i=0;i<classes.size();i++)
+        {
+            
