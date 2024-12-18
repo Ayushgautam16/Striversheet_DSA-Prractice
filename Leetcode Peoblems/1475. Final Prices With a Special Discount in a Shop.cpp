495c0e8c39a67e52861a08665df6c259fd1664d5
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-12-18 22:09:47 +0530

    1

--- Leetcode Peoblems/1475. Final Prices With a Special Discount in a Shop.cpp
+++ Leetcode Peoblems/1475. Final Prices With a Special Discount in a Shop.cpp
@@ -6,4 +6,10 @@ public:
 
         vector<int> ans;
 
-        
+        for (int i = 0; i < prices.size(); i++){
+
+            bool flag = false;
+
+            for (int j = i + 1; j < prices.size(); j++){
+
+                
