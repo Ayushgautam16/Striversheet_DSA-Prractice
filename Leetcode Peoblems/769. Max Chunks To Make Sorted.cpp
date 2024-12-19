4640ab98c5e95b1b6bc1abe04634a2533a14f498
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-12-19 22:08:47 +0530

    1

--- Leetcode Peoblems/769. Max Chunks To Make Sorted.cpp
+++ Leetcode Peoblems/769. Max Chunks To Make Sorted.cpp
@@ -3,4 +3,9 @@
 
 class Solution {
 public:
-    
+   bool canBeChunked (vector<int>&arr, int i, int j) {
+        int count = j-i+1;
+        int valcount = 0;
+        for (int k=i; k<=j; k++) {
+            if (arr[k] >=i && arr[k] <= j) valcount++;
+        
