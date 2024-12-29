d731dd35be729f95ac340b51598ae36130b3f93e
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-12-29 23:21:55 +0530

    1

--- Leetcode Peoblems/qts.cpp
+++ Leetcode Peoblems/qts.cpp
@@ -1,4 +1,8 @@
 class Solution {
 public:
     int numWays(vector<string>& words, string target) {
+        vector<vector<int>> dp(words[0].size(), vector<int>(target.size(), -1));
+        vector<vector<int>> charFrequency(words[0].size(), vector<int>(26, 0));
+
+        // Store the frequency of every character at every index.
         
