d4eda1ef99f6315149f79198d7dd7d78e96c4609
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-12-29 23:22:07 +0530

    1

--- Leetcode Peoblems/qts.cpp
+++ Leetcode Peoblems/qts.cpp
@@ -5,4 +5,8 @@ public:
         vector<vector<int>> charFrequency(words[0].size(), vector<int>(26, 0));
 
         // Store the frequency of every character at every index.
-        
+        for (int i = 0; i < words.size(); i++) {
+            for (int j = 0; j < words[0].size(); j++) {
+                int character = words[i][j] - 'a';
+                charFrequency[j][character]++;
+            
