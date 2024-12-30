b329d200af05dd2c19b31f768d72c4def997ae17
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-12-30 21:46:02 +0530

    1

--- Leetcode Peoblems/2466. Count Ways To Build Good Strings.cpp
+++ Leetcode Peoblems/2466. Count Ways To Build Good Strings.cpp
@@ -3,4 +3,6 @@ public:
     int countGoodStrings(int low, int high, int zero, int one) {
         int mod = 1e9 + 7;
         vector<int> dp(high + 1, 0);
+        dp[0] = 1; // Base case: 1 way to create an empty string.
+
         
