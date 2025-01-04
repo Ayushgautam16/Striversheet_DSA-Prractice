2b35a51a9ee76aa805dcd6c1f9a782ab9f83385a
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2025-01-05 00:37:12 +0530

    1

--- Leetcode Peoblems/1930. Unique Length-3 Palindromic Subsequences.cpp
+++ Leetcode Peoblems/1930. Unique Length-3 Palindromic Subsequences.cpp
@@ -1,2 +1,11 @@
 // 1930. Unique Length-3 Palindromic Subsequences
 
+class Solution {
+public:
+    int countPalindromicSubsequence(string s) {
+        unordered_set<char> letters;
+        for (char c : s) {
+            letters.insert(c);
+        }
+        
+        
